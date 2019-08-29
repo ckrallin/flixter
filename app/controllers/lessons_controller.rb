18b152047c207a368522457a63a1190d30c371d5
Conner Krall <conner.krall@gmail.com>
2019-08-29 17:12:17 +0000

    permissions lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
   def show
     
   end
 
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to course_path(current_lesson.section.course), alert: "NOT ENROLLED"
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
